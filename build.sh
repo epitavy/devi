fe5c89bce21e89abb7ea82151c2f1bf94d57b45f
Eliaz Pitavy <eliaz.pitavy@epita.fr>
2021-03-04 16:44:38 +0100

    Toto
    Signed-off-by: Eliaz Pitavy <eliaz.pitavy@epita.fr>

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo "Creating build dir"
+mkdir -p build && cd build
+echo "conan: install deps"
+conan install -u .. --build missing -s compiler.libcxx=libstdc++11 -s compiler.cppstd=20
+echo "cmake: generate build scripts"
+cmake .. -G Ninja -DPYTHON_EXECUTABLE=$(which python3) -DCMAKE_BUILD_TYPE=Release
+echo "cmake: launch build"
+cmake --build . --parallel --config Release
+echo "cpack: create artefacts"
+cpack -G ZIP -G TGZ .    
+echo "**********************"
+echo "BUILD COMPLETE"
+echo "**********************" 
